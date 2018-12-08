8df32cc8d4a0ca23b635ee4a24154fe42f723c25
Stefan Koch <programming@stefan-koch.name>
2018-12-08 12:56:14 +0100

    restart supervisor to make new conf files available

--- postroot.sh
+++ postroot.sh
@@ -3,4 +3,5 @@ pluginfoldername=$3
 cp /opt/loxberry/data/plugins/$pluginfoldername/supervisor.conf /etc/supervisor/conf.d/internetradio.conf
 
 supervisorctl reread
+supervisorctl reload
 supervisorctl start internetradio
